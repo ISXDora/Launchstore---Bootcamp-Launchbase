23e1eb92f241ddc0a68cec8a9093f52ff55e33ab
Isadora Xavier <isinhasena@gmail.com>
2022-04-30 19:26:23 -0300

    criação de procedure no banco e date sem UTC

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,8 +2,8 @@ module.exports = {
     date(timestamp){
         const date = new Date(timestamp)
         const year = date.getUTCFullYear()
-        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
-        const day = `0${date.getUTCDate()}`.slice(-2)
+        const month = `0${date.getMonth() + 1}`.slice(-2)
+        const day = `0${date.getDate()}`.slice(-2)
         const hour = date.getHours();
         const minutes = date.getMinutes();
 
